b7595de2df7322784c4e9875a6836e567fca00d8
Eduardo Bernardino <eduardomoura.moura@gmail.com>
2019-08-01 08:56:27 -0300

    Impementação

--- ListaEncadeada/ListaEncadeada.cpp
+++ ListaEncadeada/ListaEncadeada.cpp
@@ -2,7 +2,96 @@
 
 using namespace std;
 
+typedef struct lista {
+    int info;
+    struct lista *prox;
+} Lista;
+
+Lista *inicializa() {
+    return NULL;
+}
+
+int vazia(Lista *l) {
+    if(l == NULL) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+Lista *insere(Lista* l, int i) {
+    Lista *novo = (Lista*) malloc(sizeof(Lista));
+    novo -> info = i;
+    novo -> prox = l;
+    return novo;
+}
+
+Lista *busca(Lista *l, int v) {
+    Lista *p;
+    for(p = l; p != NULL; p = p -> prox) {
+        if(p -> info == v) return p;
+    }
+    return NULL;
+}
+
+Lista *retira(Lista *l, int v) {
+    Lista *ant = NULL;
+    Lista *p = l;
+
+    while(p != NULL && p -> info != v) {
+        ant = p;
+        p = p -> prox;
+    }
+
+    if(p == NULL) return l;
+
+    if(ant == NULL) {
+        l = p -> prox;
+    } else {
+        ant -> prox = p -> prox;
+    }
+    free(p);
+
+    return l;
+}
+
+void libera(Lista *l) {
+    Lista *p = l;
+    while(p != NULL) {
+        Lista *t = p -> prox;
+
+        free(p);
+        p = t;
+    }
+}
+
+void imprime(Lista *l) {
+    Lista *p;
+    cout << "INFO: ";
+    for(p = l; p != NULL; p = p -> prox){
+        cout << p -> info << " ";
+    }
+}
+
 int main() {
+
+    Lista *l;
+    l = inicializa();
+    
+    l = insere(l, 23);
+    l = insere(l, 45);
+    l = insere(l, 56);
+    l = insere(l, 78);
+    imprime(l);
+    
+    l = retira(l, 78);
+    cout << "\nREMOVEU 78" << endl;
+    imprime(l);
+    l = retira(l, 45);
+    cout << "\nREMOVEU 45" << endl;
+    imprime(l);
+
+    libera(l);
 
     return 0;
 }
